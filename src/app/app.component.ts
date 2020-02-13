7aba29557f89b073c09dcabcbba70575e8bc866c
Kreggy <filip.makrlik@gmail.com>
2020-02-13 23:22:38 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/Http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'News';
+  private articles = [];
+  private search = "";
+  private adress = "https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=7efbfb56607a43ee84875a0b3d0b1de0";
+
+  clickedButton() {
+    this.adress = "https://newsapi.org/v2/everything?q="+this.search+"&sortBy=publishedAt&apiKey=7efbfb56607a43ee84875a0b3d0b1de0";
+    console.log(this.adress);
+    this.articles = [];
+    this.httpClient
+    .get(this.adress)
+    .subscribe(
+      (data: any) => {
+        this.articles = data["articles"];
+        console.log("nice vol. 2");
+        }, (error) => {
+          console.log("fuck")
+      }
+    );
+  }
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient
+    .get(this.adress)
+    .subscribe(
+      (data: any) => {
+        this.articles = data["articles"];
+        console.log("nice vol. 2");
+        }, (error) => {
+          console.log("fuck vol. 2");
+      }
+    );
+  }
 }
